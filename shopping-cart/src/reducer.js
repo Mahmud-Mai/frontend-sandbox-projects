7b1f8cd37c6c019c105c439b09344a723380cd86
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2023-02-27 19:51:30 +0100

    Shopping cart project; Set up conditional to calculate Total price in reducer.js

--- shopping-cart/src/reducer.js
+++ shopping-cart/src/reducer.js
@@ -48,6 +48,30 @@ const reducer = (state, action) => {
             }
         default: console.log("default decrease item");;
     }
+
+    switch(action.type) {
+        case 'GET_TOTAL':
+            let { total, amount } = state.cart.reduce(
+                (cartTotal, cartItem) => {
+                  const { price, amount } = cartItem
+                  const itemTotal = price * amount
+
+                  cartTotal.total += itemTotal
+                  cartTotal.amount += amount
+                  return cartTotal
+                },
+                {
+                  total: 0,
+                  amount: 0,
+                }
+              )
+              total = parseFloat(total.toFixed(2))
+
+              return { ...state, total, amount }
+
+                default: return console.log('Total amount changed');
+    }
+
     return state;
 }
 
